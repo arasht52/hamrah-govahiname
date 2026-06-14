5a57a2267d4965e87ceaae13763970b96933ec1d
arasht52 <arasht52@gmail.com>
2026-06-14 17:50:14 +0200

    Create colors.js

---
+++ src/theme/colors.js
@@ -0,0 +1,30 @@
+export const COLORS = {
+  bg: "#F2F7F1",
+  bgSoft: "#E8F6E8",
+
+  card: "#FFFFFF",
+  cardSoft: "#F4FBF4",
+
+  green: "#168A3A",
+  greenDark: "#0F6B2A",
+  greenLight: "#D7EADB",
+
+  border: "#BBD7C0",
+  borderSoft: "#D7EADB",
+
+  text: "#111827",
+  textSoft: "#374151",
+  muted: "#64736A",
+
+  danger: "#DC2626",
+  dangerSoft: "#FEF2F2",
+  dangerBorder: "#FCA5A5",
+
+  warning: "#FACC15",
+  warningSoft: "#FFF7ED",
+  warningBorder: "#FDBA74",
+  warningText: "#9A3412",
+
+  white: "#FFFFFF",
+  black: "#111827"
+};
