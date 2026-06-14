f7d46517717e9e519fd9f521a1f857c5220c4870
arasht52 <arasht52@gmail.com>
2026-06-14 17:17:36 +0200

    Update imageQuestions.js

--- src/data/imageQuestions.js
+++ src/data/imageQuestions.js
@@ -1,1 +1,34 @@
-export const IMAGE_QUESTIONS = [];
+// سوالات تصویری آزمون گواهینامه آلمان
+
+export const IMAGE_QUESTIONS = [
+  /*
+  نمونه ساختار:
+
+  {
+    id: 1001,
+    topic: "Vorfahrt",
+    points: 5,
+
+    q_de: "Wer hat Vorfahrt?",
+    q_fa: "حق تقدم با کدام وسیله است؟",
+
+    image_url: null,
+
+    opts_de: [
+      "Das rote Fahrzeug",
+      "Das blaue Fahrzeug",
+      "Ich"
+    ],
+
+    opts_fa: [
+      "خودروی قرمز",
+      "خودروی آبی",
+      "وسیله نقلیه من"
+    ],
+
+    ok: [1],
+
+    exp_fa: "خودروی آبی حق تقدم دارد."
+  }
+  */
+];
