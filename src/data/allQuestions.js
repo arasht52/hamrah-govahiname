5deb2ded1822362a0ec5a5d50a40b4d9bfdac13b
arasht52 <arasht52@gmail.com>
2026-06-15 23:24:46 +0200

    Update allQuestions.js

--- src/data/allQuestions.js
+++ src/data/allQuestions.js
@@ -1,7 +1,9 @@
 import { QUESTIONS } from "./questions";
 import { IMAGE_QUESTIONS } from "./imageQuestions";
+import { CUSTOM_QUESTIONS } from "./customQuestions";
 
 export const ALL_QUESTIONS = [
   ...QUESTIONS,
-  ...IMAGE_QUESTIONS
+  ...IMAGE_QUESTIONS,
+  ...CUSTOM_QUESTIONS
 ];
